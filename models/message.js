982fa125f764695212acee717fa200cee28d00fe
Alicia Poindexter <alcampbel@gmail.com>
2015-11-13 14:11:29 -0800

    add mongolab

--- models/message.js
+++ models/message.js
@@ -1,4 +1,10 @@
 'use strict';
 
-far fs = require('fs');
+var mongoose = require('mongoose');
 
+var userSchema = mongoose.Schema({
+  username: {type: String, required: true, unique: true},
+  createdAt: { type: Date, default: new Date() }
+});
+
+var User = mongoose.model('User', userSchema
--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,14 @@ var router = express.Router();
 
 var Message = require('../models/message');
 
+router.get('/', function(req, res) {
+  res.render("index");
+});
+
 router.post('/', function (req, res) {
   var input = req.body;
   Message.create(input, function(err) {
-    if(err) return res.status(400).send(err);
-    res.send('index');
+    if(err) return res.status(400).send(err.message);
+    res.send(messages);
   });
 });
